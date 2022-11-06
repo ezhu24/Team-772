73c8b563141316007177ad3b38b09f8f67ba9c19
ezhu24 <117597437+ezhu24@users.noreply.github.com>
2022-11-06 15:29:58 -0500

    Add files via upload

---
+++ TestAutonomous.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import java.lang.annotation.Target;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class TestAutonomous{
+
+public static void main(){
+
+Team772BlueSide robot = new Team772BlueSide();
+
+robot.ForwardBackward(538, 0.75);
+
+
+}
+
+}
